82d57f96d7910f1b7c85ecdfc8430b3398f737c8
KIM <vkn123@naver.com>
2022-09-30 04:12:49 +0900

    Update settings.py

--- Khuton2022/Khuton2022/settings.py
+++ Khuton2022/Khuton2022/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Khuton2022.urls'
-CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000','http://ec2-50-18-22-205.us-west-1.compute.amazonaws.com:8080','http://ec2-50-18-22-205.us-west-1.compute.amazonaws.com','localhost:8080')
 CORS_ALLOW_CREDENTIALS = True
 TEMPLATES = [
     {
