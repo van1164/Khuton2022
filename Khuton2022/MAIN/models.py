5d9815709e27ae438c13b35aa3302bbd3fb63f31
KIM <vkn123@naver.com>
2022-09-30 02:44:09 +0900

    df

--- Khuton2022/MAIN/models.py
+++ Khuton2022/MAIN/models.py
@@ -1,3 +1,9 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class TEST(models.Model):
+    test= models.CharField(max_length=50)
+    content = models.TextField()
+    updated_at = models.DateTimeField(auto_now =True)
--- Khuton2022/MAIN/serializers.py
+++ Khuton2022/MAIN/serializers.py
@@ -1,2 +1,6 @@
 from rest_framework import serializers
 
+
+class TESTING(serializers.ModelSerializer):
+    class Meta:
+        model = TEST
