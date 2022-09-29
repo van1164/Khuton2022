ab805698775832c6741a3711eab47572f8b71cef
KIM <vkn123@naver.com>
2022-09-30 03:42:21 +0900

    Update models.py

--- Khuton2022/MAIN/models.py
+++ Khuton2022/MAIN/models.py
@@ -4,10 +4,6 @@ from django.forms import CharField
 
 # Create your models here.
 
-class TEST(models.Model):
-    test= models.CharField(max_length=50)
-    content = models.TextField()
-    updated_at = models.CharField(max_length=50)
     
 class TEST2(models.Model):
     test= models.CharField(max_length=50)
