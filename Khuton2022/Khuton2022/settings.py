df2514e13f7627d4f8064311cd529cf4d00fa5da
KIM <vkn123@naver.com>
2022-09-30 02:45:54 +0900

    Update settings.py

--- Khuton2022/Khuton2022/settings.py
+++ Khuton2022/Khuton2022/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'Khuton2022.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
