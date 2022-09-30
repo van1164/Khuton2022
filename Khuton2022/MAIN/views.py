f404daaeaf134f445696673a09bd7f4b1c1ac288
KIM <vkn123@naver.com>
2022-10-01 03:53:52 +0900

    Update views.py

--- Khuton2022/MAIN/views.py
+++ Khuton2022/MAIN/views.py
@@ -134,19 +134,29 @@ def create_timetable(request):
     def cosine_similarity(a, b):
         return np.dot(a, b) / (np.linalg.norm(a) * (np.linalg.norm(b)))
 
+    sim_list = []
     max = 0
     index = 0
     for a,i in data2.iterrows():
         sim = cosine_similarity(sunwu, data2.loc[a][1:])
+        sim_list.append([i.student_ID, sim])
     if sim > max:
         max = sim 
         index = a
+        
+    sim_list = sorted(sim_list, key = lambda x: x[1], reverse=True)
+    print(sim_list[:4])
 
     return_list = []
-    for a, i in enumerate(zip(data2.iloc[index][2:], sunwu)):
-        if i[0] <= current_semester and i[0] > 0 and i[1] == 0:
-            return_list.append(subjectss[a])
-    print(return_list)
+    return_list2 = []
+    for i in sim_list[:4]:
+        for a, i in enumerate(zip(data2.iloc[i[0]][2:], sunwu)):
+            if i[0] <= current_semester and i[0] > 0 and i[1] == 0:
+                return_list.append(subjectss[a])
+        return_list2.append(return_list)
+            
+    print(return_list2)
+    
     result = []
     for i in return_list:
         if everyinfo_table.objects.filter(name=i).exists():
