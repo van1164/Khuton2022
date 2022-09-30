b6217aeecbe42fc79e80193acd6fe14b71fcaf48
KIM <vkn123@naver.com>
2022-10-01 00:32:55 +0900

    Update views.py

--- Khuton2022/MAIN/views.py
+++ Khuton2022/MAIN/views.py
@@ -126,8 +126,24 @@ def create_timetable(request):
             i.실전기계학습]
       )
     
-    print(pd.DataFrame(final_list, columns = subjectss))
-    
+    data2= pd.DataFrame(final_list, columns = subjectss)
+    sunwu = [1,1,1,4,3,3,2,1,2,3,4,3,4,4,0,0,4,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    current_semester = 5
+
+    def cosine_similarity(a, b):
+        return np.dot(a, b) / (np.linalg.norm(a) * (np.linalg.norm(b)))
+
+    max = 0
+    index = 0
+    for a,i in data2.iterrows():
+        sim = cosine_similarity(sunwu, data2.loc[a][2:])
+    if sim > max:
+        max = sim 
+        index = a
+
+    for a, i in enumerate(zip(data2.iloc[index][2:], sunwu)):
+        if i[0] <= current_semester and i[0] > 0 and i[1] == 0:
+            print(subjectss[a])
     
     
     
